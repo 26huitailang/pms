c212a109ef30e92add15f776de3882d937e9d072
peter <26huitailang@gmail.com>
2017-10-25 23:06:41 +0800

    add 新的bootstrap模板融入项目，后续只需引用修改
    1. 完善数据库和views

--- pms/blueprints/admin/views.py
+++ pms/blueprints/admin/views.py
@@ -5,27 +5,33 @@ from flask_login import login_required
 from pms.blueprints.user.models import User
 from pms.blueprints.admin.models import Dashboard
 
-
 admin = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
 
 
-@admin.before_request
-@login_required
-def before_request():
-    pass
+# @admin.before_request
+# @login_required
+# def before_request():
+#     pass
 
 
-@admin.route('/users', defaults={'page': 1})
-@admin.route('/users/page/<int:page>')
-def index(page):
-    users = User.query.paginate(page, per_page=15)
-
-    return render_template('admin/user/index.html', users=users)
-
-
-@admin.route('')
-def dashboard():
+@admin.route('/')
+@admin.route('/index')
+def index():
     group_and_count_users = Dashboard.group_and_count_users()
 
     return render_template('admin/page/dashboard.html',
                            group_and_count_users=group_and_count_users)
+
+
+@admin.route('/projects')
+def projects():
+    return 'Projects'
+
+
+@admin.route('/users', defaults={'page': 1})
+@admin.route('/users/page/<int:page>')
+def users(page):
+    users = User.query.paginate(page, per_page=15)
+
+    return render_template('admin/user/users.html', users=users)
+
